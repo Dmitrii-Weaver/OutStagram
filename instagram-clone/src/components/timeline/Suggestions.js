47e6fd5898895c1ea2907ab8aa8c5c6b439a25ce
Weaver <cordies1106@gmail.com>
2024-01-11 03:26:30 +0200

    suggestions added

--- instagram-clone/src/components/timeline/Suggestions.js
+++ instagram-clone/src/components/timeline/Suggestions.js
@@ -1,9 +1,62 @@
 import React from 'react'
 import "./Suggestions.css"
+import { Avatar } from '@mui/material'
 
 function Suggestions() {
   return (
-    <div>Suggestions</div>
+    <div className='suggestions'>
+      <div className='suggestions_title'>Suggestions for you</div>
+      <div className='suggestions_usernames'>
+        <div className='suggestions_username'>
+          <div className='username_left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username_info'>
+              <span className='username'>TestUser123</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow_button'>Follow</button>
+        </div>
+        <div className='suggestions_username'>
+          <div className='username_left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username_info'>
+              <span className='username'>TestUser123</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow_button'>Follow</button>
+        </div>
+        <div className='suggestions_username'>
+          <div className='username_left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username_info'>
+              <span className='username'>TestUser123</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow_button'>Follow</button>
+        </div>
+        <div className='suggestions_username'>
+          <div className='username_left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username_info'>
+              <span className='username'>TestUser123</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow_button'>Follow</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
