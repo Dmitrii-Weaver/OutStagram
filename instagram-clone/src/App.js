5972b681074aee212f3a8d2dc3b447709227985c
Weaver <cordies1106@gmail.com>
2024-01-18 07:17:37 +0200

    redux set up and attached

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Homepage from './Homepage';
 import Authentification from './components/authentification/Authentification';
+import {useSelector} from "react-redux"
 
 function App() {
+  const user = useSelector(state => state.data.user.user)
+
   return (
     <div className="app">
-      {/*<Homepage />*/}
-      <Authentification/>
+      {user ? (<Homepage/>) : (<Authentification/>)}
     </div>
   );
 }
