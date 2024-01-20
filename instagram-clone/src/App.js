a58ba1c8d98f02caf1cf94afe55283373ae7b61f
Weaver <cordies1106@gmail.com>
2024-01-20 14:04:36 +0200

    sign up finished

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,14 +1,36 @@
+import { useEffect } from 'react';
 import './App.css';
 import Homepage from './Homepage';
 import Authentification from './components/authentification/Authentification';
-import {useSelector} from "react-redux"
+import { loginUser, setLoading } from "./features/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { auth } from './firebase';
+
 
 function App() {
+  
   const user = useSelector(state => state.data.user.user)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(loginUser({
+          uid: authUser.uid,
+          username: authUser.displayName,
+          email: authUser.email,
+        }))
+        dispatch(setLoading(false))
+      }
+      else {
+        console.log("user not logged in")
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
-      {user ? (<Homepage/>) : (<Authentification/>)}
+      {user ? (<Homepage />) : (<Authentification />)}
     </div>
   );
 }
